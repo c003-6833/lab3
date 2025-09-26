ddcc0fd77b330e4ea2419a3f277dcdfac7503000
Amy O'Neill <c00306833@setu.ie>
2025-09-26 08:29:35 +0000

    lab

--- task2.py
+++ task2.py
@@ -1,19 +1,34 @@
 import re
 from collections import defaultdict
 
-def ip_parse(line: str) -> str | None:
-    # Regex for IPv4 addresses
-    match = re.search(r"\d+\.\d+\.\d+\.\d+", line)
-    if match:
-        return match.group(0)
+LOGFILE = "sample_auth_small.log"  # change filename if needed
+
+def ip_parser(line):
+    """
+    looks for the substring ' port ' and returns the following port number.
+    Returns None if no matching substring found.
+    """
+    if " from " in line:
+        parts = line.split() # splits the line into tokens, seperates by spaces by default
+        try:
+            anchor = parts.index("from")    # Find the position of the token "port", our anchor
+            port = parts[anchor+1]          # the port value will be next token, anchor+1
+            return port.strip()             # strip any trailing punctuation
+
+        except (ValueError, IndexError):
+            return None
+
     return None
+
+LOGFILE = "sample_auth_small.log"
+
 
 counts = defaultdict(int)  # Create a dictionary to keep track of IPs
 
 with open("sample_auth_small.log") as f:
     for line in f:
         if "Failed password" in line or "Invalid user" in line:
-            ip = ip_parse(line)
+            ip = ip_parser(line)
             if ip:
                 counts[ip] += 1
 
